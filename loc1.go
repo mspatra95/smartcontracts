22fb8d37158ae8262a1e216f589f4360676ed439
Suman <msumanp@hexaware.com>
2018-09-05 09:45:07 +0530

    1st Commit LOC1

---
+++ loc1.go
@@ -0,0 +1,39 @@
+/*
+Letter Of Creditdocument
+A letter of credit is a document from a bank that guarantees payment.
+*/
+ package main
+/* Imports 
+* 2 specific Hyperledger Fabric specific libraries for Smart Contracts
+*/
+import ( 
+ github.com/hyperledger/fabric/core/chaincode/shim
+ sc github.com/hyperledger/fabric/protos/peer 
+ )
+
+// Define the Smart Contract structure
+	type SmartContract struct {
+	}
+
+
+
+//Invoke a chaincode function 
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response { 
+function, args := stub.GetFunctionAndParameters() 
+fmt.Println("invoke is running " + function) 
+if function == locstep1 {
+return t.locstep1(stub, args)	
+}
+fmt.Println("invoke did not find func: " + function)
+return shim.Error("Received unknown function invocation: " + function)
+}
+
+
+func main() {
+
+	// Create a new Smart Contract
+	err := shim.Start(new(SmartContract))
+	if err != nil {
+		fmt.Printf("Error creating new Smart Contract: %s", err)
+	}
+}
